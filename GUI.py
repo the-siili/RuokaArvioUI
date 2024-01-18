e505cb13640581a3737ce9d02e4c899d3f9238c6
Siili <51329090+the-siili@users.noreply.github.com>
2024-01-18 09:10:55 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,6 +1,15 @@
 import tkinter as tk
+import time
 
 
+
+
+def pressed(txt):
+    txt.config(text='Kiitos Vastauksesta!')
+    #send shit
+    time.sleep(2)
+
+    txt.config(text="Miten arvioisit tämän päivän ruoan?")
 
 
 
@@ -11,8 +20,13 @@ root = tk.Tk()
 root.geometry("800x480")
 
 text = tk.Label(text="Miten arvioisit tämän päivän ruoan?",font=("Arial", 35))
-desc = tk.Label(text="Vastaathan vain kerran päivässä. \nArviot eivät mitenkään vaikuta koulun tuleviin ruokavalintoihin.",font=("Arial", 15))
+desc = tk.Label(text="Vastaathan vain kerran päivässä. \nArviot eivät mitenkään vaikuta koulun tuleviin ruokavalintoihin.",font=("Arial", 15), fg="grey")
+
+
+
+
 
 text.place(x=30, y=140)
 desc.place(x=120, y=220)
+
 root.mainloop()
